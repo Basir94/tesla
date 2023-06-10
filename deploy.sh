31499351c77e37f45a91c7c55fe11472046e60d0
Simon Legah <legah2000@gmail.com>
2023-06-10 17:22:02 -0400

    conflict 2 resolved

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,9 @@ deploy python apps
 deploy in the dev enviroment
 deployment5 master
 deployment4
+HEAD
 dev
+deploy to staging
+echo "good job"
+tested and accepted
+6c8aac8 (tested and approve by the client qa team)
